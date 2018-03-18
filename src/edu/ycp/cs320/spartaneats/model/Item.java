6df6bdc9c33e33b6d6b1ebb60471135728ef5ffe
Samantha Kiser <skiser@ycp.edu>
2018-03-18 15:01:17 -0400

    fixed csvs and item class

--- src/edu/ycp/cs320/spartaneats/model/Item.java
+++ src/edu/ycp/cs320/spartaneats/model/Item.java
@@ -1,40 +1,60 @@
 package edu.ycp.cs320.spartaneats.model;
 
 public class Item {
-	private String MainItem;
+	private String mainItem;
 	private String Side;
 	private String drink;
 	private int count;
 	private double price;
 	
 	//item has a main item and side
-	public Item(String MainItem, String Side) {
-		this.setMainItem(MainItem);
+	public Item(String mainItem, String Side, double price, int count) {
+		this.setMainItem(mainItem);
 		this.setSide(Side);
+		this.setPrice(price);
+		this.setCount(count);
+	}
+
+	//number of items
+	private void setCount(int Count) {
+		this.count = Count;
+	}
+
+	//price of items
+	private void setPrice(double Price) {
+		this.price = Price;
 	}
 
 	//get the side
-	public String getSide() {
-		return Side;
+	public String getSide(String side) {
+		return side;
 	}
 
 	//set the side
 	public void setSide(String side) {
-		Side = side;
+		this.Side = side;
 	}
 
 	//get the main item
-	public String getMainItem() {
-		return MainItem;
+	public String getmainItem() {
+		return mainitem;
 	}
 
 	//set the main item
 	public void setMainItem(String mainItem) {
-		MainItem = mainItem;
+		this.mainItem = mainItem;
 	}
 	
 	public void getPrice(double price) {
 		this.price = price;
+	}
+
+	public String getDrink() {
+		return drink;
+	}
+
+	public void setDrink(String drink) {
+		this.drink = drink;
 	}
 	
 }
--- src/edu/ycp/cs320/spartaneats/model/Order.java
+++ src/edu/ycp/cs320/spartaneats/model/Order.java
@@ -43,5 +43,6 @@ public class Order  {
 		Pickup = pickup;
 	}
 	
+
 	
 }
