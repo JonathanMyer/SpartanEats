214ed36d4fb6c63cf98ed87fd9adfe5a3202d25b
JmyerYCP <jmyer@ycp.edu>
2018-04-04 23:19:44 -0400

    Separted create order for item and drink sections

--- src/edu/ycp/cs320/spartaneats/model/Inventory.java
+++ src/edu/ycp/cs320/spartaneats/model/Inventory.java
@@ -1,12 +1,25 @@
 package edu.ycp.cs320.spartaneats.model;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
+
+import edu.ycp.cs320.spartaneats.persist.DerbyDatabase;
 
 public class Inventory {
-	private ArrayList<Item> selectedItems;
+	private List<Item> selectedItems;
+	private List<Drink> selectedDrinks;
+	private DerbyDatabase db = null;
 	
 	public Inventory() {
-		selectedItems = new ArrayList<Item>();
+		db = new DerbyDatabase();
+		try {
+			selectedItems = db.findAllItems();
+			selectedDrinks = db.findAllDrinks();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	
@@ -20,6 +33,15 @@ public class Inventory {
 		selectedItems.remove(Item);
 	}
 	
+	public void addDrink(Drink drink) {
+		selectedDrinks.add(drink);
+	}
+	
+	public void removeItem(Drink drink) {
+		selectedDrinks.remove(drink);
+	}
+	
+	
 	// returns the item with given itemname
 	public Item getItem(String item) {
 		for (Item i: selectedItems) {
@@ -31,8 +53,26 @@ public class Inventory {
 		return null;
 	}
 	
-	public ArrayList<Item> getItemList(){
+	public Drink getDrink(String drink) {
+		for (Drink i: selectedDrinks) {
+			if (i.getItemName().equals(drink)) {
+				System.out.println("found Drink");
+				return i;
+			}
+		}
+		return null;
+	}
+	
+	public List<Item> getItemList(){
 		return this.selectedItems;
 	}
+
+
+	public List<Drink> getDrinkList() {
+		return selectedDrinks;
+	}
+
+
+
 
 }
